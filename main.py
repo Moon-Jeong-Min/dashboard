b52015a3de4ab82af3dc07929d2e3a7f0a598ba5
Moon-Jeong-Min <wjdals2004v@gmail.com>
2024-08-31 11:25:17 +0900

    날짜 범위 선택 및 선택된 날짜 데이터 표시하는 테이블 추가

--- main.py
+++ main.py
@@ -8,7 +8,16 @@ def main():
 
     df = load_data()
 
-    st.write(df)
+    # 특정 날짜 범위 선택
+    st.subheader('Select Date Range')
+    df['Date'] = pd.to_datetime(df['Date'])
+    start_date = st.date_input('Start date', df['Date'].min())
+    end_date = st.date_input('Start date', df['Date'].max())
+
+    ranged_df = df[(df['Date'] >= pd.to_datetime(start_date)) &
+                    (df['Date'] <= pd.to_datetime(end_date))]
+    ranged_df = ranged_df.reset_index(drop=True)
+    st.table(ranged_df)
 
 if __name__ == '__main__':
     main()
