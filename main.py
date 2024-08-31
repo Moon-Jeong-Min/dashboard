0b8dd577bc477d86af39f16e9906e0f4c965e797
Moon-Jeong-Min <wjdals2004v@gmail.com>
2024-08-31 14:42:41 +0900

    주가 차트 추가

--- main.py
+++ main.py
@@ -21,5 +21,8 @@ def main():
     ranged_df = ranged_df.reset_index(drop=True)
     st.table(ranged_df)
 
+    st.subheader('주가 변동 차트')
+    st.line_chart(ranged_df.set_index('Date')['Close'])
+
 if __name__ == '__main__':
     main()
